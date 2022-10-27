10eef18789a11c7a0082eea2930053d8caf0299a
Travis Hunsberger <tfhunsberger@gmail.com>
2022-10-27 15:19:21 -0400

    added logic to set background of squares on mouseover to black on window load

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('.container');
 const buttonsContainer = document.querySelector('.buttons')
 
+const greyButton = document.createElement('button');
+const rgbButton = document.createElement('button');
+
 function createDivs(columns, rows) {
     for (let i = 0; i < (columns * rows); i++) {
         const div = document.createElement('div');
@@ -11,3 +14,13 @@ function createDivs(columns, rows) {
     }
 }
 createDivs(16,16);
+
+window.onload = () => {
+    const divs = document.querySelectorAll('.squares');
+    // console.log(divs);
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', (event) => {
+            event.target.style.background = "purple";
+        })
+    })
+}
